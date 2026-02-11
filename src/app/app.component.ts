ae079f775da4118edf55c0ed58807e609a529ce5
Nathan <nathanlgd14@gmail.com>
2026-02-11 15:05:40 +0100

    Add food list retrieval and update app configuration for HTTP client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-dev-cloud';
+
+  http: HttpClient;
+  foodList: any[] = [];
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  ngOnInit() {
+    try {
+      this.http.get('http://localhost:3000/api/food').subscribe((data: any) => {
+        console.log('Retrieved data successfully', data);
+        this.foodList = data;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
